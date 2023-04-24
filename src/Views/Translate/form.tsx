abfa1359e2c2322bfb97dfa647661b54e948fee5
paulneo <jpaulneo2k@gmail.com>
2023-04-24 10:19:55 -0500

    add

---
+++ src/Views/Translate/form.tsx
@@ -0,0 +1,12 @@
+
+import { Input } from '@chakra-ui/react'
+
+const Form  = () => {
+  return (
+    <Input placeholder='medium size' size='md' />
+
+  )
+}
+
+
+export default Form
--- src/Views/Translate/index.tsx
+++ src/Views/Translate/index.tsx
@@ -1,21 +1,30 @@
-import { Box, Button, Text, useColorMode } from '@chakra-ui/react';
+import { Box, useColorMode,Flex } from '@chakra-ui/react';
+import Form from './form'
 
 const Translate = () => {
 	const { toggleColorMode } = useColorMode();
 	return (
-		<Box>
-			<Text
-				bgGradient="linear(to-l, #7928CA, #FF0080)"
-				bgClip="text"
-				fontSize="4xl"
-				fontWeight="extrabold"
-			>
-				Google translate
-			</Text>
-			<Button colorScheme='blue' variant='solid' onClick={toggleColorMode}>
-				change color
-			</Button>
-		</Box>
+			<Flex  align="center" justify="center" height={'100vh'}>
+
+				{/* <Text
+					bgGradient="linear(to-l, green.600, yellow.800)"
+					bgClip="text"
+					fontSize="4xl"
+					fontWeight="extrabold"
+				>
+					Google translate
+				</Text>
+				<Button colorScheme='blue' variant='solid' onClick={toggleColorMode}>
+					change color
+				</Button> */}
+				<Box>
+					<Form/>
+				</Box>
+				<Box>
+					<Form/>
+					
+				</Box>
+			</Flex>
 	);
 };
 
