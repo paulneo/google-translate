27145c653ca4babe6d458f2ee89a9093b84fd643
paulneo <jpaulneo2k@gmail.com>
2023-05-02 21:02:06 -0500

    add form

--- src/Views/Translate/translateCard.tsx
+++ src/Views/Translate/translateCard.tsx
@@ -1,10 +1,20 @@
-import { Card, CardBody, Textarea } from '@chakra-ui/react';
+import { Card, CardBody, Flex, Icon, Textarea } from '@chakra-ui/react';
+import { FaMicrophone, FaVolumeUp } from 'react-icons/fa';
 
 const TranslateCard = () => {
 	return (
 		<Card mt={5} width={'100%'}>
 			<CardBody>
-				<Textarea variant='unstyled' />
+				<Textarea
+					variant='unstyled'
+					resize={'none'}
+					fontSize={28}
+					fontWeight={'medium'}
+				/>
+				<Flex align={'center'} gap={5} mt='8'>
+					<Icon as={FaMicrophone} boxSize={5} color={'gray.500'} />
+					<Icon as={FaVolumeUp} boxSize={5} color={'gray.500'} />
+				</Flex>
 			</CardBody>
 		</Card>
 	);
